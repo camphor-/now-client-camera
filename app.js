cae2e5e43a622151d6c833bc72e1fb98527a9a57
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-02-07 01:59:39 +0900

    Add initial files

---
+++ app.js
@@ -0,0 +1,7 @@
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
+socket.on('connect', function(){});
+socket.on('event', function(data){});
+socket.on('disconnect', function(){});
