53f860a217d71f826da641c58c6d7a5dcde0794c
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-02-10 20:10:13 +0900

    Implement Socket.IO

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
+import {argv} from 'yargs';
+import debugLogger from 'debug';
+import fs from 'fs';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+const debug = debugLogger('application');
 
-socket.on('connect', function(){});
-socket.on('event', function(data){});
-socket.on('disconnect', function(){});
+const NAMESPACE = '/camera';
+
+const url = argv._[0] || 'http://localhost:3000';
+
+debug(`Server URL: ${url}`);
+debug(`Namespace: ${NAMESPACE}`);
+
+const socket = io(`${url}${NAMESPACE}`);
+
+socket.on('connect', () => {
+  debug('connected');
+});
+
+socket.on('disconnect', () => {
+  debug('disconnected');
+});
+
+socket.on('take picture', (data) => {
+  debug('take picture');
+  const responseEvent = data.responseEvent;
+
+  // TODO: Take a picture w/ Raspberry Pi
+  fs.readFile('./test.jpg', (err, data) => {
+    if (err) {
+      socket.emit(responseEvent, {
+        success: false
+      });
+      return;
+    }
+
+    socket.emit(responseEvent, {
+      success: true
+    });
+  });
+});
