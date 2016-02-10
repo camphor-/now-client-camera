8791c6c1ee923c948b9b2e3de0ed23c6ae20a18b
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-02-10 23:12:41 +0900

    Add sample mode

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 import {argv} from 'yargs';
 import debugLogger from 'debug';
 import {execSync} from 'child_process';
+import {readFile} from 'fs';
 import io from 'socket.io-client';
 
-const debug = debugLogger('application');
-
+// Constants
 const NAMESPACE = '/camera';
 
-const url = argv._[0] || 'http://localhost:3000';
+// Logger
+const debug = debugLogger('application');
 
+// Parse command line arguments
+let parseMode = (mode) => {
+  const _mode = (mode || '').toLowerCase();
+  switch (_mode) {
+  case 'raspistill':
+    return _mode;
+  case 'sample':
+    return _mode;
+  default:
+    return 'sample'
+  }
+};
+
+const url = argv._[0] || 'http://localhost:3000';
+const mode = parseMode(argv.mode);
+
+// Start
 debug(`Server URL: ${url}`);
 debug(`Namespace: ${NAMESPACE}`);
+debug(`Mode: ${mode}`);
 
 const socket = io(`${url}${NAMESPACE}`);
 
@@ -22,12 +41,52 @@ socket.on('disconnect', () => {
   debug('disconnected');
 });
 
+let raspistill = (responseEvent) => {
+  try {
+    const picture = execSync('raspistill -w 320 -h 240 -n -e jpg -o -');
+    debug('success');
+    socket.emit(responseEvent, {
+      success: true,
+      data: picture
+    });
+  } catch (e) {
+    const message = e.toString();
+    debug(`error: ${message}`);
+    socket.emit(responseEvent, {
+      success: false,
+      message: message
+    });
+  }
+};
+
+let sampleFile = (responseEvent) => {
+  readFile('./test.jpg', (err, data) => {
+    if (err) {
+      socket.emit(responseEvent, {
+        success: false,
+        message: err.toString()
+      });
+      return;
+    }
+    socket.emit(responseEvent, {
+      success: true,
+      data: data
+    });
+  });
+};
+
 socket.on('take picture', (data) => {
   debug('take picture');
   const responseEvent = data.responseEvent;
-  const picture = execSync('raspistill -w 320 -h 240 -n -e jpg -o -');
-  socket.emit(responseEvent, {
-    success: true,
-    data: picture
-  });
+  switch (mode) {
+  case 'sample':
+    sampleFile(responseEvent);
+    break;
+  case 'raspistill':
+    raspistill(responseEvent);
+    break;
+  default:
+    sampleFile(responseEvent);
+    break;
+  }
 });
